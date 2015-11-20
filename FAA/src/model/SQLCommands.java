aa787eefb961735ba692e977576cb8ad88204f96
Debra <Debra@150.250.212.71>
2015-11-20 09:51:39 -0500

    added updated jdbc and library

--- FAA/src/model/SQLCommands.java
+++ FAA/src/model/SQLCommands.java
@@ -1,47 +1,58 @@
 package model;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rowan_user
+ */
 import java.sql.*;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class SQLCommands {
 	
-	private ArrayList<InstanceACList> List_Of_Data;
-	private static Connection conn;
-
-	public SQLCommands(String url, String user, String ext) throws SQLException {
-		List_Of_Data = new ArrayList<InstanceACList>();
-		conn = DriverManager.getConnection(url, user, ext);
-	}
-
-	public Connection getConn() {
-		return conn;
-	}
-
-	public void setConn(Connection conn) {
-		this.conn = conn;
+        private String returnedString;
+	//private ArrayList<InstanceOfData> List_Of_Data;
+	
+        public String getReturnedString(){
+            return returnedString;
+        }
+        
+	public void SQLCommands() {
+		//List_Of_Data = new ArrayList<InstanceOfData>();
 	}
 	
 	public void clear() {
-		List_Of_Data = new ArrayList<InstanceACList>();
+		//List_Of_Data = new ArrayList<InstanceOfData>();
 	}
 	
 	public boolean isClear() {
-		if(List_Of_Data == null || List_Of_Data.size() < 1) {
-			return true;
-		}
+		//if(List_Of_Data == null || List_Of_Data.size() < 1) {
+			//return true;
+		//}
 		return false;
 	}
 
-	public static void viewTable(String dbName) throws SQLException {
+	public static void viewTable(Connection con, String dbName) throws SQLException {
 		    Statement stmt = null;
-		    String query = "select ACID " +
-		                   "from " + dbName + ".AC_LISTRW15150804";
+                    StringBuilder sb = new StringBuilder();
+                    // SELECT COUNT(*) FROM AC_LISTRW15150804 WHERE AC_NUM LIKE '%59%'
+		    String query = "SELECT AC_NUM, ACID FROM AC_LISTRW15150804";
+                    String query2 = "Select ";
 		    try {
-		        stmt = conn.createStatement();
-		        ResultSet rs = stmt.executeQuery(query);
-		        while (rs.next()) {
-		            String acid = rs.getString("ACID");
-		            System.out.println(acid);
+		        stmt = con.createStatement();
+		        ResultSet listSet = stmt.executeQuery(query);
+                        ResultSet trackSet = stmt.executeQuery(query2);
+                        sb.append("acNum, acID\n");
+		        while (listSet.next()) {
+                            String acNum = listSet.getString("AC_NUM");
+		            String acID = listSet.getString("ACID");
+		            sb.append(acNum + ", " + acID + "\n");
 		        }
 		    } catch (SQLException e ) {
 		        System.err.println(e.toString());
@@ -50,33 +61,15 @@ public class SQLCommands {
 		    }
 		}
 	
-	public static void viewTable(String dbName, String colName) throws SQLException {
+	public static void viewTable(Connection con, String dbName, String colName) throws SQLException {
 	    Statement stmt = null;
 	    String query = "select " + colName +
-	                   " from " + dbName + ".AC_LISTRW15150804";
+	                   " from " + dbName + ".AC_LIST";
 	    try {
-	        stmt = conn.createStatement();
+	        stmt = con.createStatement();
 	        ResultSet rs = stmt.executeQuery(query);
 	        while (rs.next()) {
-	            String acid = rs.getString(colName);
-	            System.out.println(acid);
-	        }
-	    } catch (SQLException e ) {
-	        System.err.println(e.toString());
-	    } finally {
-	        if (stmt != null) { stmt.close(); }
-	    }
-	}
-	
-	public static void viewTable(String dbName, String colName, String tableName) throws SQLException {
-	    Statement stmt = null;
-	    String query = "select " + colName +
-	                   " from " + dbName + "."+ tableName;
-	    try {
-	        stmt = conn.createStatement();
-	        ResultSet rs = stmt.executeQuery(query);
-	        while (rs.next()) {
-	            String acid = rs.getString(colName);
+	            String acid = rs.getString("colName");
 	            System.out.println(acid);
 	        }
 	    } catch (SQLException e ) {
--- FAA/src/model/SQLDriver.java
+++ FAA/src/model/SQLDriver.java
@@ -1,14 +1,46 @@
 package model;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rowan_user
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.StringBuilder;
 import java.sql.*;
 
 public class SQLDriver {
 
-	public void main(String args[]) throws SQLException {
-		
-		String url = "????";
-		SQLCommands sqc = new SQLCommands(url, "uret01", "rowan");
-		
-		sqc.viewTable("pariphae_cablocal");
+	public void main(String args[]) throws SQLException, IOException {
+
+                
+                File file = new File("/home/rowan_user/custom.txt");
+                
+                if(!file.exists()){
+                    file.createNewFile();
+
+                
+                }
+                FileWriter fileWriter = new FileWriter(file);
+                
+                fileWriter.write("test");
+                
+                
+            
+
+		SQLCommands sqc = new SQLCommands();
+		System.out.println("test");
+		String url = "jdbc:sqlplus:cablocal";
+		Connection con = DriverManager.getConnection(url, "uret01", "rowan");
+		sqc.viewTable(con, "pariphae_cablocal");
+                
 	}
 }
